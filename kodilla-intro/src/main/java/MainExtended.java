cec5cfc3ab54ca227d71ae85df58aef75c0c1131
Anna Kozina <kajdaszuk.ania@gmail.com>
2022-05-29 16:34:06 +0200

    Main(Extended)

---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,16 @@
+public class MainExtended {
+    public static void main(String [] args) {
+        String myName = "Ania";
+        int myAge = 30;
+        double myHeight = 1.70;
+        char myGrade = 'G';
+        int myClass = 4;
+        String myColour = "Green";
+        System.out.println(myName);
+        System.out.println(myAge);
+        System.out.println(myHeight);
+        System.out.println(myGrade);
+        System.out.println(myClass);
+        System.out.println(myColour);
+    }
+}
