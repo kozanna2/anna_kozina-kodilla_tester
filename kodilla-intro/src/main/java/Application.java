b2c2ab84b8ace1c8d8d85415fb468ae1e561e92b
Anna Kozina <kajdaszuk.ania@gmail.com>
2022-06-14 14:47:27 +0200

    Application2

--- kodilla-intro/src/main/java/Application.java
+++ kodilla-intro/src/main/java/Application.java
@@ -1,2 +1,36 @@
-package PACKAGE_NAME;public class Application {
+public class Application {
+
+    private String name;
+    private double age;
+    private double height;
+
+    public Application(String name, double age, double height) {
+        this.name = name;
+        this.age = age;
+        this.height = height;
+
+        Application user = new Application("Adam", 40.5, 178);
+        user.ifOlderAndTaller();
+    }
+
+    public double getAge() {
+        return age;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void ifOlderAndTaller() {
+        if (this.name != null) {
+            if (getAge() > 30 && getHeight() > 160) {
+                System.out.println("User is older than 30 and taller than 160cm");
+            } else {
+                System.out.println("User is 30 (or younger) or 160cm (or shorter).");
+            }
+        } else {
+            return;
+        }
+    }
 }
+
